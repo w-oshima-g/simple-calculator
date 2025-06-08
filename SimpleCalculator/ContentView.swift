7d021ade15c7fad0ce91864764ef86156de0171d
w-oshima <wataru.oshima.g@gmail.com>
2025-06-08 12:14:45 +0900

    四則演算追加

--- SimpleCalculator/ContentView.swift
+++ SimpleCalculator/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var number1 = ""
     @State private var number2 = ""
-    @State private var result  = 0
+    @State private var result  = ""
     
     var body: some View {
         VStack(spacing: 20) {
@@ -26,15 +26,46 @@ struct ContentView: View {
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             
-            Button("計算する") {
-                let n1 = Int(number1) ?? 0
-                let n2 = Int(number2) ?? 0
-                result = n1 + n2
+            HStack {
+                Button("+") {
+                    calculate { $0 + $1 }
+                }
+                .buttonStyle()
+                
+                Button("-") {
+                    calculate { $0 - $1 }
+                }
+                .buttonStyle()
             }
-            .padding()
-            .background(Color.blue)
-            .foregroundColor(.white)
-            .cornerRadius(10)
+            
+            HStack {
+                Button("×") {
+                    calculate { $0 * $1 }
+                }
+                .buttonStyle()
+                
+                Button("÷") {
+                    calculate {
+                        if $1 != 0 {
+                            return $0 / $1
+                        } else {
+                            result = "エラー：0で割れません"
+                            return nil
+                        }
+                    }
+                }
+                .buttonStyle()
+            }
+//
+//            Button("計算する") {
+//                let n1 = Int(number1) ?? 0
+//                let n2 = Int(number2) ?? 0
+//                result = n1 + n2
+//            }
+//            .padding()
+//            .background(Color.blue)
+//            .foregroundColor(.white)
+//            .cornerRadius(10)
             
             Text("結果: \(result)")
                 .font(.title2)
@@ -42,8 +73,27 @@ struct ContentView: View {
         }
         .padding()
     }
+    
+    private func calculate(_ operation: (Int, Int) -> Int?) {
+        guard let n1 = Int(number1), let n2 = Int(number2) else {
+            result = "数値を正しく入力してください"
+            return
+        }
+        if let res = operation(n1, n2) {
+            result = String(res)
+        }
+    }
 }
 
+extension Button {
+    func buttonStyle() -> some View {
+        self
+            .frame(width: 60, height:  40)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
 #Preview {
     ContentView()
 }
